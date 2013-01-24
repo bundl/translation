8f5e2dbbf32b43daae4af264cae845310b530165
Brooke Bryan <brooke@justdevelop.it>
2013-01-24 11:16:50 +0000

    Translation Bundle

---
+++ src/Bundl/Translation/TranslationBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author  brooke.bryan
+ */
+
+namespace Bundl\Translation;
+
+use Cubex\Bundle\Bundle;
+use Cubex\Events\Event;
+use Cubex\Events\EventManager;
+
+class TranslationBundle extends Bundle
+{
+  public function init($initialiser = null)
+  {
+  }
+}
